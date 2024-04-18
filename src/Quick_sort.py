569e7099ddef441eae430309dd33eb368ec8ac29
Thrylos1 <116742874+Thrylos1@users.noreply.github.com>
2024-04-18 16:49:13 -0400

    Addded unit test and 2d functionality

--- src/Quick_sort.py
+++ src/Quick_sort.py
@@ -3,17 +3,17 @@
 
 
 def partition(arr, lo, hi):
-    pivot = arr[hi]
+    pivot = arr[hi][2]
     up = lo
     down = hi
     while (up < down):
         for j in (up, hi):
-            if arr[up] > pivot:
+            if arr[up][2] > pivot:
                 break
             up += 1
         
         for j in (hi, lo, -1):
-            if arr[down] < pivot:
+            if arr[down][2] < pivot:
                 break
             down -= 1
         
@@ -36,3 +36,19 @@ def quick_sort(array2d, low, high):
         pivot = partition(array2d, low, high)
         quick_sort(array2d, low, pivot-1)
         quick_sort(array2d, pivot+1, high)
+
+def unit_test():
+    sampleList = [
+        ["song5", 5],
+        ["song3", 3],
+        ["song6", 6],
+        ["song1", 1],
+        ["song3", 3],
+        ["song8", 8],
+    ]
+
+    sortedList = quick_sort(sampleList, 1, len(sampleList))
+
+    print(sortedList)
+
+unit_test()
